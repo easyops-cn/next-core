303a74f40e2616a2ad93a31fa78de4a070da2930
stevewang <stevewang@easyops.cn>
2019-09-23 15:50:18 +0800

    fix(): set `skipLibCheck: true` during schema generating
    Refs INFRA-434

--- packages/brick-types/scripts/post-build.js
+++ packages/brick-types/scripts/post-build.js
@@ -2,9 +2,12 @@ const path = require("path");
 const fs = require("fs");
 const TJS = require("typescript-json-schema");
 
-const program = TJS.getProgramFromFiles([
-  path.resolve(__dirname, "../src/manifest.ts")
-]);
+const program = TJS.getProgramFromFiles(
+  [path.resolve(__dirname, "../src/manifest.ts")],
+  {
+    skipLibCheck: true
+  }
+);
 const schema = TJS.generateSchema(program, "Storyboard");
 
 const schemaDir = path.resolve(__dirname, "../.schema");
