812de471fb64009260969dab410c6dc3231e5bfe
stevewang <stevewang@easyops.cn>
2019-10-08 10:08:56 +0800

    chore(brick-wrapper): fix tests using mocked implementations

--- packages/brick-kit/src/BrickWrapper.spec.tsx
+++ packages/brick-kit/src/BrickWrapper.spec.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
-
-jest.mock("antd/es/locale/zh_CN");
-jest.mock("antd/es/locale/en_US");
 import { BrickWrapper } from "./BrickWrapper";
+
+jest.mock("i18next", () => ({ language: "zh" }));
+jest.mock("antd/es/locale/zh_CN", () => "zh_CN");
+jest.mock("antd/es/locale/en_US", () => "en_US");
 
 describe("brick wrapper", () => {
   it("should work", () => {
