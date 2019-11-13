5e9f16a16f119bbcc4808b3ae6b3543d59a8602b
stevewang <stevewang@easyops.cn>
2019-11-13 13:48:22 +0800

    fix(): fix an error when no devDependencies
    INFRA-434

--- packages/build-config-factory/src/validateDeps.js
+++ packages/build-config-factory/src/validateDeps.js
@@ -15,7 +15,7 @@ module.exports = function ensureDeps(scope) {
           "/*` should not have any `dependencies` or `peerDependencies`, use `devDependencies` instead."
       );
     }
-    const devDependencies = Object.keys(packageJson.devDependencies);
+    const devDependencies = Object.keys(packageJson.devDependencies || {});
     if (
       devDependencies.some(
         name =>
@@ -45,7 +45,7 @@ module.exports = function ensureDeps(scope) {
           "/*` should not have any `dependencies`, use `peerDependencies` for `@bricks/*` and `@templates/*`, use `devDependencies` for others."
       );
     }
-    const peerDependencies = Object.keys(packageJson.peerDependencies);
+    const peerDependencies = Object.keys(packageJson.peerDependencies || {});
     if (
       peerDependencies.some(
         name => !(name.startsWith("@bricks/") || name.startsWith("@templates/"))
@@ -57,7 +57,7 @@ module.exports = function ensureDeps(scope) {
           "/*` should only contain `@bricks/*` and `@templates/*` in `peerDependencies`, use `devDependencies` for others."
       );
     }
-    const devDependencies = Object.keys(packageJson.devDependencies);
+    const devDependencies = Object.keys(packageJson.devDependencies || {});
     if (
       devDependencies.some(
         name => name.startsWith("@bricks/") || name.startsWith("@templates/")
