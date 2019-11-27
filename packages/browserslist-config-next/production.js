589b630d65767e9600ed2942cd4c6779f5cf25d8
stevewang <stevewang@easyops.cn>
2019-11-27 17:41:38 +0800

    feat(): add @easyops/browserslist-config-next
    INFRA-434

---
+++ packages/browserslist-config-next/production.js
@@ -0,0 +1,11 @@
+// Ref https://github.com/browserslist/browserslist#shareable-configs
+module.exports = [
+  // Desktop browsers
+  // with decent display: grid support: https://caniuse.com/#feat=css-grid
+  // with proper class support: https://bugzilla.mozilla.org/show_bug.cgi?id=1216630
+  // with proper web components support: https://caniuse.com/#search=web%20components
+  "Edge >= 76",
+  "Chrome >= 57",
+  "Firefox >= 63",
+  "Safari >= 10.1"
+];
