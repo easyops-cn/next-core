24f99e0b79c638ae51df33b3100e5c1af0483212
William Cai <williamcai@easyops.cn>
2020-01-19 10:48:24 +0800

    fix(build-config-factory): generateDeps 兼容没有 peerDependencies 的场景
    Refs CONTAINER-523

--- packages/build-config-factory/src/generateDeps.js
+++ packages/build-config-factory/src/generateDeps.js
@@ -2,7 +2,10 @@ const path = require("path");
 const fs = require("fs-extra");
 const yaml = require("js-yaml");
 
-const scopeToSuffix = new Map([["@bricks", "NB"], ["@templates", "NT"]]);
+const scopeToSuffix = new Map([
+  ["@bricks", "NB"],
+  ["@templates", "NT"]
+]);
 
 module.exports = function generateDeps() {
   const packageJson = require(path.join(process.cwd(), "package.json"));
@@ -13,21 +16,22 @@ module.exports = function generateDeps() {
   }
   const conf = yaml.safeLoad(fs.readFileSync(confPath, "utf8"));
   const existedDeps = new Set(conf.dependencies.map(dep => dep.name));
-  conf.dependencies.push(
-    ...Object.entries(peerDependencies)
-      .map(([name, version]) => {
-        const [scope, pkg] = name.split("/");
-        if (!scopeToSuffix.has(scope)) {
-          throw new Error(`unexpected peer dependency: ${name}`);
-        }
-        const suffix = scopeToSuffix.get(scope);
-        return {
-          name: `${pkg}-${suffix}`,
-          version
-        };
-      })
-      .filter(dep => !existedDeps.has(dep.name))
-  );
+  peerDependencies &&
+    conf.dependencies.push(
+      ...Object.entries(peerDependencies)
+        .map(([name, version]) => {
+          const [scope, pkg] = name.split("/");
+          if (!scopeToSuffix.has(scope)) {
+            throw new Error(`unexpected peer dependency: ${name}`);
+          }
+          const suffix = scopeToSuffix.get(scope);
+          return {
+            name: `${pkg}-${suffix}`,
+            version
+          };
+        })
+        .filter(dep => !existedDeps.has(dep.name))
+    );
   const content = yaml.safeDump(conf);
   fs.outputFileSync(
     path.join(process.cwd(), "deploy/package.conf.yaml"),
