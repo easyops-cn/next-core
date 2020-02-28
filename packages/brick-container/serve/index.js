5151ffcd06f47281dddbf5ed4ae592cb960f8126
stevewang <stevewang@easyops.cn>
2020-02-28 09:38:20 +0800

    fix(): fix auto-remote not working
    INFRA-434

--- packages/brick-container/serve/index.js
+++ packages/brick-container/serve/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const httpProxyMiddleware = require("http-proxy-middleware");
 const { throttle } = require("lodash");
 const chokidar = require("chokidar");
+const chalk = require("chalk");
 const getEnv = require("./getEnv");
 const serveLocal = require("./serveLocal");
 const getProxies = require("./getProxies");
@@ -72,7 +73,10 @@ app.use(serveIndexHtml);
 
 app.listen(port);
 
-console.log(`Started serving at: http://localhost:${port}${env.publicPath}`);
+console.log(
+  chalk.bold.green("Started serving at:"),
+  `http://localhost:${port}${env.publicPath}`
+);
 
 // 建立 websocket 连接支持自动刷新
 const WebSocket = require("ws");
