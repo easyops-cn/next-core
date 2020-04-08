d1d62ef77e09ded81cdcc36a935be7631d1df67d
stevewang <stevewang@easyops.cn>
2020-04-08 11:27:51 +0800

    chore(test): increase coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -7,30 +7,30 @@ module.exports = {
     "\\.spec\\.[jt]sx?$",
     "/packages/.*-config-factory/",
     "/scripts/",
-    "/__jest__/"
+    "/__jest__/",
   ],
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      statements: 91.5,
-      branches: 85.04,
-      functions: 95.21,
-      lines: 91.41
-    }
+      statements: 95.31,
+      branches: 88.46,
+      functions: 97.38,
+      lines: 95.28,
+    },
   },
   coverageDirectory: "<rootDir>/.coverage",
   coverageReporters: ["lcov", "text-summary"],
   transform: {
-    "^.+\\.[jt]sx?$": "babel-jest"
+    "^.+\\.[jt]sx?$": "babel-jest",
   },
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "/template/"],
   moduleNameMapper: {
-    "\\.svg": "<rootDir>/__mocks__/svgrMock.js"
+    "\\.svg": "<rootDir>/__mocks__/svgrMock.js",
   },
   // Ref https://github.com/facebook/jest/issues/2070#issuecomment-431706685
   // Todo(steve): remove next line when issue fixed.
   modulePathIgnorePatterns: ["<rootDir>/.*/__mocks__"],
   // Use jsdom >= 14 which supports MutationObserver
   testEnvironment: "jest-environment-jsdom-fifteen",
-  timers: "fake"
+  timers: "fake",
 };
