d15f3fe26894ac2afd7e4de699e506e4f74b5477
stevewang <stevewang@easyops.cn>
2020-07-01 16:44:00 +0800

    chore(): increase size limit of dll

--- .size-limit.js
+++ .size-limit.js
@@ -13,7 +13,7 @@ module.exports = [
   },
   {
     path: "packages/brick-container/dist/dll.js",
-    limit: "1.25 MB",
+    limit: "1.26 MB",
   },
   {
     path: "packages/brick-container/dist/main.*.js",
