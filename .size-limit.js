bd57110c3201a90396d5eadf7416c034b77942df
alexchen <alexchen@easyops.cn>
2020-07-06 11:16:06 +0800

    feat(brick-icons): add icons for the blueprint
    Closes BLUEPRINT-41

--- .size-limit.js
+++ .size-limit.js
@@ -29,7 +29,7 @@ module.exports = [
   },
   {
     path: "packages/brick-icons/dist/index.esm.js",
-    limit: "125 KB",
+    limit: "130 KB",
   },
   {
     path: "packages/brick-kit/dist/index.esm.js",
