227f1aa72c78f6b2bca1eb79ad75e8601f36edff
stevewang <stevewang@easyops.cn>
2020-07-09 17:32:42 +0800

    fix(): throw error at test to avoid new conflicted properties
    INFRA-434

--- packages/brick-kit/src/decorators/utils.ts
+++ packages/brick-kit/src/decorators/utils.ts
@@ -9,15 +9,18 @@ export function warnNativeHtmlElementProperty(
   // istanbul ignore if
   if (
     key in HTMLElement.prototype &&
-    !options?.__deprecated_and_for_compatibility_only &&
-    // istanbul ignore next
-    process.env.NODE_ENV !== "test"
+    !options?.__deprecated_and_for_compatibility_only
   ) {
+    const message = `"${key}" is a native HTMLElement property, and is deprecated to be used as a brick property.`;
+    // istanbul ignore if
+    if (process.env.NODE_ENV === "test") {
+      // Allow existed properties to work.
+      // But throw error at test, to avoid new conflicted properties.
+      throw new Error(message);
+    }
     // eslint-disable-next-line no-console
     console[
       getRuntime().getFeatureFlags()["development-mode"] ? "error" : "warn"
-    ](
-      `"${key}" is a native HTMLElement property, and is deprecated to be used as a brick property.`
-    );
+    ](message);
   }
 }
