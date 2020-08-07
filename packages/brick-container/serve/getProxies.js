6d292dd41f1e9795089a252800ebfbc53608b86a
stevewang <stevewang@easyops.cn>
2020-08-07 14:50:15 +0800

    fix(serve): allow unsecure proxy for https which is probably self-signed
    INFRA-434

--- packages/brick-container/serve/getProxies.js
+++ packages/brick-container/serve/getProxies.js
@@ -53,12 +53,18 @@ module.exports = (env) => {
           const { data } = result;
           if (localMicroApps.length > 0 || mockedMicroApps.length > 0) {
             data.storyboards = mockedMicroApps
-              .map((id) => getSingleStoryboard(env, id, true, {
-                brief: true
-              }))
-              .concat(localMicroApps.map((id) => getSingleStoryboard(env, id, false, {
-                brief: true
-              })))
+              .map((id) =>
+                getSingleStoryboard(env, id, true, {
+                  brief: true,
+                })
+              )
+              .concat(
+                localMicroApps.map((id) =>
+                  getSingleStoryboard(env, id, false, {
+                    brief: true,
+                  })
+                )
+              )
               .filter(Boolean)
               .concat(
                 data.storyboards.filter(
@@ -107,6 +113,7 @@ module.exports = (env) => {
     : proxyPaths.reduce((acc, seg) => {
         acc[`${publicPath}${seg}`] = {
           target: server,
+          secure: false,
           changeOrigin: true,
           pathRewrite: pathRewriteFactory(seg),
           ...(seg === "api" ? apiProxyOptions : {}),
