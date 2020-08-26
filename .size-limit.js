67b4ebf7f3473799b99e0e2d2bd91d5cc435bfad
momomo <momomo@easyops.cn>
2020-08-26 12:25:30 +0800

    chore(): switch icon add size-limit

--- .size-limit.js
+++ .size-limit.js
@@ -29,7 +29,7 @@ module.exports = [
   },
   {
     path: "packages/brick-icons/dist/index.esm.js",
-    limit: "130 KB",
+    limit: "131 KB",
   },
   {
     path: "packages/brick-kit/dist/index.esm.js",
