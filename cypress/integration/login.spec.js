6a2a3fa425a03c7fc77a972ac198316fe31c0f36
stevewang <stevewang@easyops.cn>
2020-09-03 10:44:18 +0800

    test(cypress): allow retries in login tests, as cookies are not always cleared between tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -22,14 +22,24 @@ describe("Login page", () => {
     cy.contains("basic-bricks\\.app-bar", "easyops");
   });
 
-  it("redirect to login page and redirect back to previous page in en", () => {
-    // We test as the real world with `--subdir`.
-    cy.visit("/next/?lng=en");
-    cy.location("pathname").should("eq", "/next/auth/login");
-    cy.get('input[placeholder="Username"]').type("easyops");
-    cy.get('input[placeholder="Password"]').type("easyops");
-    cy.get('button[type="submit"]').click();
-    cy.url().should("eq", `${Cypress.config("baseUrl")}/next/?lng=en`);
-    cy.contains("basic-bricks\\.app-bar", "easyops");
-  });
+  it(
+    "redirect to login page and redirect back to previous page in en",
+    {
+      // Cookies may be not always cleared between tests.
+      // Ref https://github.com/cypress-io/cypress/issues/781
+      retries: {
+        runMode: 1,
+      },
+    },
+    () => {
+      // We test as the real world with `--subdir`.
+      cy.visit("/next/?lng=en");
+      cy.location("pathname").should("eq", "/next/auth/login");
+      cy.get('input[placeholder="Username"]').type("easyops");
+      cy.get('input[placeholder="Password"]').type("easyops");
+      cy.get('button[type="submit"]').click();
+      cy.url().should("eq", `${Cypress.config("baseUrl")}/next/?lng=en`);
+      cy.contains("basic-bricks\\.app-bar", "easyops");
+    }
+  );
 });
