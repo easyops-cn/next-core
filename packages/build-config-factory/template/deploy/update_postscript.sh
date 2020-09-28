bc5b5a826270a74a35c3801857b1a3b763cb3e95
winsenzhu <winsenzhu@easyops.cn>
2020-09-28 18:35:36 +0800

    chore(update_deploy_script): 更新和安装脚本,org优先取环境变量，cloned_install_path变量是否存在来判断是否使用克隆模式 Refs NEXT_BUILDER-401

--- packages/build-config-factory/template/deploy/update_postscript.sh
+++ packages/build-config-factory/template/deploy/update_postscript.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # 为保证一致，直接用 install_postscript.sh
-bash ./deploy/install_postscript.sh $1
+bash ./deploy/install_postscript.sh
+[[ $? -ne 0 ]] && echo "install postscript error, exit" && exit 1
+
+exit 0
