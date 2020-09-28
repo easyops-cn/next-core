1310c23767af5f92e0cf1b7fd661c05cf48ead92
stevewang <stevewang@easyops.cn>
2020-09-28 17:51:34 +0800

    fix(): update renovate file filters
    INFRA-434

--- packages/dev-dependencies/patches/index.js
+++ packages/dev-dependencies/patches/index.js
@@ -2,3 +2,4 @@ exports.majorBrickNext = require("./majorBrickNext");
 exports.updateLernaAllowBranch = require("./updateLernaAllowBranch");
 exports.updateMRTemplates = require("./updateMRTemplates");
 exports.updateBuildStories = require("./updateBuildStories");
+exports.updateRenovateFileFilters = require("./updateRenovateFileFilters");
