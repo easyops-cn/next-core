36c0a9d497a8d7baba08bf67598980b429b8e365
stevewang <stevewang@easyops.cn>
2020-11-09 18:06:11 +0800

    fix(): integrate with pipe list
    NEXT_BUILDER-440

--- packages/next-docs/scripts/sync.js
+++ packages/next-docs/scripts/sync.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const fs = require("fs-extra");
 
+const pipesPath = path.dirname(
+  require.resolve("@easyops-cn/brick-next-pipes/package.json")
+);
 const tempPath = path.join(__dirname, "../temp");
 
+fs.copySync(path.join(pipesPath, "api"), tempPath);
+
 fs.copySync(
-  path.join(
-    path.dirname(require.resolve("@easyops-cn/brick-next-pipes/package.json")),
-    "api"
-  ),
-  tempPath
+  path.join(pipesPath, "dist/pipes.json"),
+  path.join(__dirname, "../pipes.json")
 );
