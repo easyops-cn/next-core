eac260587c4644890e8566e8f85819fad8af6369
stevewang <stevewang@easyops.cn>
2021-01-14 14:44:51 +0800

    fix(): refine editor template
    NEXT_BUILDER-637

--- packages/brick-scripts/template/editor-brick/$kebab-brick-last-name$.editor.spec.tsx
+++ packages/brick-scripts/template/editor-brick/$kebab-brick-last-name$.editor.spec.tsx
@@ -3,16 +3,17 @@ import { shallow } from "enzyme";
 import * as helper from "@easyops/editor-bricks-helper";
 import { $PascalBrickName$Editor } from "./$kebab-brick-last-name$.editor";
 
-jest.spyOn(helper, "useBuilderNode").mockReturnValue({
-  type: "brick",
-  id: "B-001",
-  brick: "$kebab-brick-last-name$",
-  alias: "my-brick",
-  parsedProperties: {},
-});
+const mockUseBuilderNode = jest.spyOn(helper, "useBuilderNode");
 
 describe("$PascalBrickName$Editor", () => {
   it("should work", () => {
+    mockUseBuilderNode.mockReturnValueOnce({
+      type: "brick",
+      id: "B-001",
+      brick: "$kebab-brick-last-name$",
+      alias: "my-brick",
+      parsedProperties: {},
+    });
     const wrapper = shallow(
       <$PascalBrickName$Editor nodeUid={1} brick="$kebab-brick-last-name$" />
     );
