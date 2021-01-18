283614407a8adb05d80441a9834dcc83f61da7ef
nlicroshan <nlicroshan@easyops.cn>
2021-01-18 10:31:39 +0800

    chore(brick-icon): add icon

--- .size-limit.js
+++ .size-limit.js
@@ -37,7 +37,7 @@ module.exports = [
   },
   {
     path: "packages/brick-icons/dist/index.esm.js",
-    limit: "222 KB",
+    limit: "227 KB",
   },
   {
     path: "packages/brick-kit/dist/index.esm.js",
