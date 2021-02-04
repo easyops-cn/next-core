fc06d258583894f72a74db19af9d83740b17d095
nlicroshan <nlicroshan@easyops.cn>
2021-02-04 15:46:25 +0800

    chore(brick-icon): configure svgr

--- packages/brick-icons/rollup.config.js
+++ packages/brick-icons/rollup.config.js
@@ -6,7 +6,11 @@ export default rollupFactory({
   umdName: "BrickIcons",
   plugins: [
     svgr({
-      exclude: ["src/icons/colored-pseudo-3d/*", "src/icons/colored-common/*"],
+      exclude: [
+        "src/icons/colored-pseudo-3d/*",
+        "src/icons/colored-common/*",
+        "src/icons/product/*",
+      ],
       svgoConfig: {
         plugins: [
           {
@@ -22,7 +26,11 @@ export default rollupFactory({
       },
     }),
     svgr({
-      include: ["src/icons/colored-pseudo-3d/*", "src/icons/colored-common/*"],
+      include: [
+        "src/icons/colored-pseudo-3d/*",
+        "src/icons/colored-common/*",
+        "src/icons/product/*",
+      ],
       svgoConfig: {
         plugins: [
           {
