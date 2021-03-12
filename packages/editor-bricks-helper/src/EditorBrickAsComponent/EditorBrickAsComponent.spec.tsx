baffa9e061fd277e30b70ed5d8c3c8459078fc9a
weareoutman <wangshenwei@qq.com>
2021-03-12 16:32:08 +0800

    fix(builder): auto determine layout of bricks with no editors

--- packages/editor-bricks-helper/src/EditorBrickAsComponent/EditorBrickAsComponent.spec.tsx
+++ packages/editor-bricks-helper/src/EditorBrickAsComponent/EditorBrickAsComponent.spec.tsx
@@ -6,6 +6,7 @@ import { UseSingleBrickConf } from "@next-core/brick-types";
 import { EditorBrickAsComponent } from "./EditorBrickAsComponent";
 import { getEditorBrick } from "./getEditorBrick";
 import { BuilderRuntimeNode, EditorSelfLayout } from "../interfaces";
+import { useBuilderData } from "../hooks/useBuilderData";
 
 jest.mock("react-dnd");
 jest.mock("@next-core/brick-kit", () => ({
@@ -18,6 +19,7 @@ jest.mock("@next-core/brick-kit", () => ({
   },
 }));
 jest.mock("./getEditorBrick");
+jest.mock("../hooks/useBuilderData");
 
 (useDrag as jest.MockedFunction<typeof useDrag>).mockReturnValue([
   { isDragging: false },
@@ -29,17 +31,22 @@ jest.mock("./getEditorBrick");
   typeof getEditorBrick
 >).mockResolvedValue("any-brick--editor");
 
+const mockUseBuilderData = useBuilderData as jest.Mock;
+
 customElements.define(
   "any-brick--editor",
   class Tmp extends HTMLElement {
     static get selfLayout(): EditorSelfLayout {
-      return EditorSelfLayout.INLINE;
+      return undefined;
     }
   }
 );
 
 describe("EditorBrickAsComponent", () => {
   it("should work", async () => {
+    mockUseBuilderData.mockReturnValue({
+      edges: [],
+    });
     const wrapper = mount(
       <EditorBrickAsComponent
         node={
@@ -58,8 +65,37 @@ describe("EditorBrickAsComponent", () => {
     wrapper.update();
     expect(wrapper.text()).toBe("BrickAsComponent(any-brick--editor,1)");
 
-    expect(wrapper.find("div").at(0).prop("className")).not.toContain(
-      "dragging"
+    const element = wrapper.find("div").at(0);
+    expect(element.hasClass("dragging")).toBe(false);
+    expect(element.hasClass("selfLayoutInline")).toBe(true);
+  });
+
+  it("should work for brick which has children", async () => {
+    mockUseBuilderData.mockReturnValue({
+      edges: [
+        {
+          parent: 1,
+        },
+      ],
+    });
+    const wrapper = mount(
+      <EditorBrickAsComponent
+        node={
+          ({
+            $$uid: 1,
+            instanceId: "instance-a",
+            id: "B-1",
+            brick: "any-brick",
+          } as Partial<BuilderRuntimeNode>) as BuilderRuntimeNode
+        }
+      />
     );
+    await act(async () => {
+      await (global as any).flushPromises();
+    });
+    wrapper.update();
+
+    const element = wrapper.find("div").at(0);
+    expect(element.hasClass("selfLayoutContainer")).toBe(true);
   });
 });
