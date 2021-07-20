b3364a0a86de91a311290397163f27625710deb0
WHChen-Alex <at.alexchen@gmail.com>
2021-07-20 12:21:39 +0800

    fix(): refine feature flags

--- packages/brick-kit/src/BrickWrapper.tsx
+++ packages/brick-kit/src/BrickWrapper.tsx
@@ -22,7 +22,9 @@ interface BrickWrapperProps {
 export function BrickWrapper(props: BrickWrapperProps): React.ReactElement {
   const locale =
     i18n.language && i18n.language.split("-")[0] === "en" ? enUS : zhCN;
-  const featureFlags = getRuntime().getFeatureFlags();
+  // istanbul ignore next
+  const featureFlags =
+    process.env.NODE_ENV === "test" ? {} : getRuntime().getFeatureFlags();
   return (
     <ErrorBoundary>
       <FeatureFlagsProvider value={featureFlags}>
