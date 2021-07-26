6e47e607c7a2f90dfc9c87dd8150c4c70298c01b
weareoutman <wangshenwei@qq.com>
2021-07-26 09:25:58 +0800

    feat(): support trackContext

---
+++ packages/brick-utils/src/trackContext.spec.ts
@@ -0,0 +1,30 @@
+import { trackContext } from "./trackContext";
+
+const consoleWarn = jest
+  .spyOn(console, "warn")
+  .mockImplementation(() => void 0);
+
+describe("trackContext", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return tracking contexts", () => {
+    expect(
+      trackContext(
+        "<% 'track context', CTX.good1(CTX['good2'], DATA.bad1, CTX[bad2]) %>"
+      )
+    ).toEqual(["good1", "good2"]);
+  });
+
+  it("should return false for non-track-ctx mode", () => {
+    expect(trackContext("<% CTX.bad %>")).toBe(false);
+    expect(trackContext("<% 'oops', 'track context', CTX.bad %>")).toBe(false);
+    expect(trackContext("<% track.CTX, CTX.bad %>")).toBe(false);
+  });
+
+  it("should return false if no CTX usage were found", () => {
+    expect(trackContext("<% 'track context', DATA.bad %>")).toBe(false);
+    expect(consoleWarn).toBeCalled();
+  });
+});
