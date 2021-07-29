b31ee0763f45ff34296d17ec134ed41608cca202
jojiang <jojiang@easyops.cn>
2021-07-29 09:42:22 +0800

    chore(): support lazy style

--- packages/webpack-config-factory/src/bricks/webpack.base.factory.js
+++ packages/webpack-config-factory/src/bricks/webpack.base.factory.js
@@ -57,218 +57,228 @@ const getImageLoaderOptions = () => ({
   ],
 });
 
-module.exports = (isForEditors) => ({
-  scope = "bricks",
-  copyFiles = [],
-  ignores = [],
-} = {}) => {
-  const cwdDirname = process.cwd();
-  const appRoot = path.join(cwdDirname, "..", "..");
-  const pkgRelativeRoot = path.relative(appRoot, cwdDirname);
-  const distPublicPath = pkgRelativeRoot
-    .split(path.sep)
-    .concat(isForEditors ? ["dist", "editors/"] : ["dist/"])
-    .join("/");
-  const imageLoaderOptions = getImageLoaderOptions();
+module.exports =
+  (isForEditors) =>
+  ({ scope = "bricks", copyFiles = [], ignores = [] } = {}) => {
+    const cwdDirname = process.cwd();
+    const appRoot = path.join(cwdDirname, "..", "..");
+    const pkgRelativeRoot = path.relative(appRoot, cwdDirname);
+    const distPublicPath = pkgRelativeRoot
+      .split(path.sep)
+      .concat(isForEditors ? ["dist", "editors/"] : ["dist/"])
+      .join("/");
+    const imageLoaderOptions = getImageLoaderOptions();
 
-  const packageJson = require(path.join(cwdDirname, "package.json"));
-  const packageName = packageJson.name.split("/")[1];
-  const dll = isForEditors
-    ? ["@next-dll/editor-bricks-helper"]
-    : Object.keys(packageJson.peerDependencies || {}).filter((name) =>
-        name.startsWith("@next-dll/")
-      );
-  const entryPair = isForEditors
-    ? ["editors", "editor-bricks/index"]
-    : ["index", "index"];
+    const packageJson = require(path.join(cwdDirname, "package.json"));
+    const packageName = packageJson.name.split("/")[1];
+    const dll = isForEditors
+      ? ["@next-dll/editor-bricks-helper"]
+      : Object.keys(packageJson.peerDependencies || {}).filter((name) =>
+          name.startsWith("@next-dll/")
+        );
+    const entryPair = isForEditors
+      ? ["editors", "editor-bricks/index"]
+      : ["index", "index"];
 
-  return {
-    context: appRoot,
-    entry: {
-      [entryPair[0]]: path.join(cwdDirname, "src", entryPair[1]),
-    },
-    output: {
-      // During webpack building, assets are written into
-      // a temporary directory `dist-editors`.
-      // And later to be merged into `dist/editors` during post-building.
-      path: path.join(cwdDirname, isForEditors ? "dist-editors" : "dist"),
-      publicPath: distPublicPath,
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-      symlinks: false,
-      alias: {
-        // This alias is a temporary fix for legacy of
-        // `@easyops/brick-http <- @sdk/* <- @next-core/brick-kit`.
-        "@easyops": "@next-core",
+    return {
+      context: appRoot,
+      entry: {
+        [entryPair[0]]: path.join(cwdDirname, "src", entryPair[1]),
       },
-    },
-    module: {
-      rules: [
-        {
-          test: /\.md$/,
-          use: [
-            {
-              loader: "html-loader",
-            },
-            {
-              loader: "markdown-loader",
-              options: {
-                highlight,
+      output: {
+        // During webpack building, assets are written into
+        // a temporary directory `dist-editors`.
+        // And later to be merged into `dist/editors` during post-building.
+        path: path.join(cwdDirname, isForEditors ? "dist-editors" : "dist"),
+        publicPath: distPublicPath,
+      },
+      resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        symlinks: false,
+        alias: {
+          // This alias is a temporary fix for legacy of
+          // `@easyops/brick-http <- @sdk/* <- @next-core/brick-kit`.
+          "@easyops": "@next-core",
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.md$/,
+            use: [
+              {
+                loader: "html-loader",
               },
-            },
-          ],
-        },
-        {
-          // Include ts, tsx, js, and jsx files.
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules|\.worker\./,
-          loader: "babel-loader",
-          options: {
-            rootMode: "upward",
-          },
-        },
-        {
-          ...imageLoaderOptions,
-          test: /\.svg$/,
-          issuer: {
-            test: /\.(ts|js)x?$/,
-          },
-          use: [
-            {
-              loader: "babel-loader",
-              options: {
-                rootMode: "upward",
-              },
-            },
-            {
-              loader: "@svgr/webpack",
-              options: {
-                babel: false,
-              },
-            },
-            ...imageLoaderOptions.use,
-          ],
-        },
-        {
-          test: /\.svg$/,
-          issuer: {
-            exclude: /\.(ts|js)x?$/,
-          },
-          ...imageLoaderOptions,
-        },
-        {
-          test: /\.(woff(2)?|ttf|eot|svga)$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "assets/[name].[hash:8].[ext]",
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(png|jpg)$/,
-          ...imageLoaderOptions,
-        },
-        {
-          test: /\.css$/,
-          exclude: /\.(module|shadow)\.css$/,
-          sideEffects: true,
-          use: ["style-loader", ...getStyleLoaders()],
-        },
-        {
-          test: /\.module\.css$/,
-          use: [
-            "style-loader",
-            ...getStyleLoaders({
-              modules: {
-                localIdentName: "[local]--[hash:base64:8]",
-              },
-            }),
-          ],
-        },
-        {
-          test: /\.shadow\.css$/,
-          sideEffects: true,
-          use: [
-            "to-string-loader",
-            ...getStyleLoaders({
-              esModule: false,
-            }),
-          ],
-        },
-        {
-          test: /\.less$/,
-          sideEffects: true,
-          use: [
-            "to-string-loader",
-            getCssLoader({
-              esModule: false,
-            }),
-            {
-              loader: "less-loader",
-              options: {
-                lessOptions: {
-                  sourceMap: false,
-                  javascriptEnabled: true,
-                  plugins: [lessReplacePlugin],
+              {
+                loader: "markdown-loader",
+                options: {
+                  highlight,
                 },
               },
+            ],
+          },
+          {
+            // Include ts, tsx, js, and jsx files.
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules|\.worker\./,
+            loader: "babel-loader",
+            options: {
+              rootMode: "upward",
             },
-          ],
-        },
-        {
-          test: /\.html$/,
-          use: "raw-loader",
-        },
-      ],
-    },
-    plugins: [
-      isForEditors
-        ? new ScanEditorBricksPlugin(packageName)
-        : scope === "templates"
-        ? new ScanTemplatesPlugin(packageName)
-        : new ScanCustomElementsPlugin(
-            packageName,
-            dll.map((name) => name.substr("@next-dll/".length))
-          ),
-      new CleanWebpackPlugin(),
-      new NextDllReferencePlugin({
-        context: appRoot,
-        // 解决该包在 `npm link` 下引用到错误的包路径的问题
-        manifest: require(require.resolve("@next-core/brick-dll", {
-          paths: [cwdDirname],
-        })),
-      }),
-      ...dll.map(
-        (name) =>
-          new NextDllReferencePlugin({
-            context: appRoot,
-            // 解决该包在 `npm link` 下引用到错误的包路径的问题
-            manifest: require(require.resolve(name, {
-              paths: [cwdDirname],
-            })),
-          })
-      ),
-      ...(copyFiles && copyFiles.length > 0
-        ? [
-            new CopyPlugin({
-              patterns: copyFiles.map((item) => ({
-                context: cwdDirname,
-                ...(typeof item === "string"
-                  ? {
-                      from: item,
-                    }
-                  : item),
+          },
+          {
+            ...imageLoaderOptions,
+            test: /\.svg$/,
+            issuer: {
+              test: /\.(ts|js)x?$/,
+            },
+            use: [
+              {
+                loader: "babel-loader",
+                options: {
+                  rootMode: "upward",
+                },
+              },
+              {
+                loader: "@svgr/webpack",
+                options: {
+                  babel: false,
+                },
+              },
+              ...imageLoaderOptions.use,
+            ],
+          },
+          {
+            test: /\.svg$/,
+            issuer: {
+              exclude: /\.(ts|js)x?$/,
+            },
+            ...imageLoaderOptions,
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svga)$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "assets/[name].[hash:8].[ext]",
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(png|jpg)$/,
+            ...imageLoaderOptions,
+          },
+          {
+            test: /\.css$/,
+            exclude: /\.(module|shadow|lazy)\.css$/,
+            sideEffects: true,
+            use: ["style-loader", ...getStyleLoaders()],
+          },
+          {
+            test: /\.module\.css$/,
+            use: [
+              "style-loader",
+              ...getStyleLoaders({
+                modules: {
+                  localIdentName: "[local]--[hash:base64:8]",
+                },
+              }),
+            ],
+          },
+          {
+            test: /\.shadow\.css$/,
+            sideEffects: true,
+            use: [
+              "to-string-loader",
+              ...getStyleLoaders({
+                esModule: false,
+              }),
+            ],
+          },
+          {
+            test: /\.lazy\.css$/,
+            use: [
+              {
+                loader: "style-loader",
+                options: {
+                  injectType: "lazyStyleTag",
+                },
+              },
+              ...getStyleLoaders(),
+            ],
+          },
+          {
+            test: /\.less$/,
+            sideEffects: true,
+            use: [
+              "to-string-loader",
+              getCssLoader({
+                esModule: false,
+              }),
+              {
+                loader: "less-loader",
+                options: {
+                  lessOptions: {
+                    sourceMap: false,
+                    javascriptEnabled: true,
+                    plugins: [lessReplacePlugin],
+                  },
+                },
+              },
+            ],
+          },
+          {
+            test: /\.html$/,
+            use: "raw-loader",
+          },
+        ],
+      },
+      plugins: [
+        isForEditors
+          ? new ScanEditorBricksPlugin(packageName)
+          : scope === "templates"
+          ? new ScanTemplatesPlugin(packageName)
+          : new ScanCustomElementsPlugin(
+              packageName,
+              dll.map((name) => name.substr("@next-dll/".length))
+            ),
+        new CleanWebpackPlugin(),
+        new NextDllReferencePlugin({
+          context: appRoot,
+          // 解决该包在 `npm link` 下引用到错误的包路径的问题
+          manifest: require(require.resolve("@next-core/brick-dll", {
+            paths: [cwdDirname],
+          })),
+        }),
+        ...dll.map(
+          (name) =>
+            new NextDllReferencePlugin({
+              context: appRoot,
+              // 解决该包在 `npm link` 下引用到错误的包路径的问题
+              manifest: require(require.resolve(name, {
+                paths: [cwdDirname],
               })),
-            }),
-          ]
-        : []),
-      ...(Array.isArray(ignores)
-        ? ignores.map((item) => new webpack.IgnorePlugin(item))
-        : []),
-    ],
+            })
+        ),
+        ...(copyFiles && copyFiles.length > 0
+          ? [
+              new CopyPlugin({
+                patterns: copyFiles.map((item) => ({
+                  context: cwdDirname,
+                  ...(typeof item === "string"
+                    ? {
+                        from: item,
+                      }
+                    : item),
+                })),
+              }),
+            ]
+          : []),
+        ...(Array.isArray(ignores)
+          ? ignores.map((item) => new webpack.IgnorePlugin(item))
+          : []),
+      ],
+    };
   };
-};
