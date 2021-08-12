6680c41e937b041af3c3889ee9382a251cd5fd13
astrid <astridli@easyop.cn>
2021-08-12 14:55:29 +0800

    chore(): add icon

--- .size-limit.js
+++ .size-limit.js
@@ -29,7 +29,7 @@ module.exports = [
   },
   {
     path: "packages/brick-container/dist/icons--*.js",
-    limit: "400 KB",
+    limit: "400.1 KB",
   },
   {
     path: "packages/brick-container/dist/polyfill.*.js",
