e7b2a8e17021bd6a1be29f4d0741276bdd269dbb
weareoutman <wangshenwei@qq.com>
2021-09-05 16:51:06 +0800

    chore(): increase size-limit of brick-utils

--- .size-limit.js
+++ .size-limit.js
@@ -53,7 +53,7 @@ module.exports = [
   },
   {
     path: "packages/brick-utils/dist/index.esm.js",
-    limit: "125 KB",
+    limit: "130 KB",
   },
   {
     path: "packages/editor-bricks-helper/dist/index.esm.js",
