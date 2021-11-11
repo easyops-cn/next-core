8cb81cc748ee14686d71bce1a86dd0fdda1037e9
weareoutman <wangshenwei@qq.com>
2021-11-11 15:03:12 +0800

    fix(): refine assets path

--- packages/easyops-illustrations/src/Illustrations.spec.tsx
+++ packages/easyops-illustrations/src/Illustrations.spec.tsx
@@ -2,23 +2,26 @@ import { getIllustration } from "./Illustrations";
 
 describe("Illustration", () => {
   it("should work", () => {
+    window.CORE_ROOT = undefined;
     const result = getIllustration({
       name: "exception",
       category: "feedback",
     });
 
-    expect(result).toBe("mockFile");
+    expect(result).toBe("assets/illustrations/mockFile");
   });
 
   it("should work with default category", () => {
+    window.CORE_ROOT = "-/core/";
     const result = getIllustration({
       name: "no-content",
     });
 
-    expect(result).toBe("mockFile");
+    expect(result).toBe("-/core/assets/illustrations/mockFile");
   });
 
   it("should return undefined when illustration not exist", () => {
+    window.CORE_ROOT = undefined;
     const result = getIllustration({
       name: "without",
     });
