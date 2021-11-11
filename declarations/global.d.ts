f288b8e468a16c697941bfff2e017038a2c62650
weareoutman <wangshenwei@qq.com>
2021-11-11 15:02:40 +0800

    chore(): refine global declarations

--- declarations/global.d.ts
+++ declarations/global.d.ts
@@ -27,5 +27,32 @@ declare module "*.png" {
 }
 
 interface Window {
+  /** For Google Analytics. */
   dataLayer?: IArguments[];
+
+  /** A map of versions of core packages. */
+  BRICK_NEXT_VERSIONS?: Record<string, string>;
+
+  /** Declare supported features currently. */
+  BRICK_NEXT_FEATURES?: string[];
+
+  /** A map of dll name to file path. */
+  DLL_PATH?: Record<string, string>;
+
+  // Variables below are for standalone micro-apps only.
+
+  /** Markup for standalone micro-apps. */
+  STANDALONE_MICRO_APPS?: boolean;
+
+  /** The app needs no auth guard.  */
+  NO_AUTH_GUARD?: boolean;
+
+  /** The public root, E.g. "hello-world/-/" */
+  PUBLIC_ROOT?: string;
+
+  /** The core root, E.g. "hello-world/-/core/" */
+  CORE_ROOT?: string;
+
+  /** The bootstrap filename, E.g. "hello-world/-/bootstrap.abc123.json" */
+  BOOTSTRAP_FILE?: string;
 }
