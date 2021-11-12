42a49f93337cc2177b5256024d82d5f632f5ab59
weareoutman <wangshenwei@qq.com>
2021-11-12 18:23:41 +0800

    test(): remove e2e tests of login page since it's not part of this repo

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -5,45 +5,9 @@ describe("Login page", () => {
     cy.logout();
   });
 
-  it("show error message if username or password is not matched", () => {
+  it("redirect to login page", () => {
     // We test as the real world with `--subdir`.
     cy.visit("/next/home/?lng=zh");
     cy.location("pathname").should("eq", "/next/auth/login");
-    cy.get('input[placeholder="用户名"]').type("easyops");
-    cy.get('input[placeholder="密码"]').type("oops");
-    cy.get('button[type="submit"]').click();
-    cy.contains("用户名（邮箱）或密码错误");
   });
-
-  it("redirect to login page and redirect back to previous page in zh", () => {
-    // We test as the real world with `--subdir`.
-    cy.visit("/next/home/?lng=zh");
-    cy.location("pathname").should("eq", "/next/auth/login");
-    cy.get('input[placeholder="用户名"]').type("easyops");
-    cy.get('input[placeholder="密码"]').type("easyops");
-    cy.get('button[type="submit"]').click();
-    cy.url().should("eq", `${Cypress.config("baseUrl")}/next/home/?lng=zh`);
-    cy.contains("basic-bricks\\.app-bar", "easyops");
-  });
-
-  it(
-    "redirect to login page and redirect back to previous page in en",
-    {
-      // Cookies may be not always cleared between tests.
-      // Ref https://github.com/cypress-io/cypress/issues/781
-      retries: {
-        runMode: 1,
-      },
-    },
-    () => {
-      // We test as the real world with `--subdir`.
-      cy.visit("/next/home/?lng=en");
-      cy.location("pathname").should("eq", "/next/auth/login");
-      cy.get('input[placeholder="Username"]').type("easyops");
-      cy.get('input[placeholder="Password"]').type("easyops");
-      cy.get('button[type="submit"]').click();
-      cy.url().should("eq", `${Cypress.config("baseUrl")}/next/home/?lng=en`);
-      cy.contains("basic-bricks\\.app-bar", "easyops");
-    }
-  );
 });
