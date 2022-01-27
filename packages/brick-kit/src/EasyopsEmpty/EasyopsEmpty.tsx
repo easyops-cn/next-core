e8f26b1b018e94b8cddb6f180fe8e74282e7d876
Wuqimenga <2949683360@qq.com>
2022-01-27 14:45:36 +0800

    fix(): fix the bug that the svg files cannot be displayed completely when in the same page
    Refs#INSTANCE-2161

--- packages/brick-kit/src/EasyopsEmpty/EasyopsEmpty.tsx
+++ packages/brick-kit/src/EasyopsEmpty/EasyopsEmpty.tsx
@@ -1,9 +1,7 @@
 import React, { useMemo } from "react";
 import { Empty } from "antd";
 import { getIllustration, IllustrationProps } from "@next-core/illustrations";
-
-import emptyImage from "../images/empty-image.svg";
-import bigEmptyImage from "../images/big-empty-image.svg";
+import { EmptySvg } from "./EmptySvg";
 
 // @internal
 export interface EasyopsEmptyProps {
@@ -22,10 +20,11 @@ export function EasyopsEmpty(props: EasyopsEmptyProps): React.ReactElement {
     () => props.illustration && getIllustration(props.illustration),
     [props.illustration]
   );
-  const _emptyImage = props.useBigEmptyImage ? bigEmptyImage : emptyImage;
-  const image = props.illustration
-    ? illustration
-    : React.createElement(_emptyImage);
+  const image = props.illustration ? (
+    illustration
+  ) : (
+    <EmptySvg isBig={props.useBigEmptyImage}></EmptySvg>
+  );
   const imageStyle =
     props.imageStyle ??
     (props.useBigEmptyImage ? undefined : { height: "60px" });
