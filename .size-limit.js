03b1244a45cc36bfe91bb1674804ac7c11378fb4
jojiang <jojiang@easyops.cn>
2022-02-21 17:25:41 +0800

    fix(): upload icon
    Refs BRICK_STORE-2475

--- .size-limit.js
+++ .size-limit.js
@@ -29,7 +29,7 @@ module.exports = [
   },
   {
     path: "packages/brick-container/dist/icons--*.js",
-    limit: "435 KB",
+    limit: "437 KB",
   },
   {
     path: "packages/brick-container/dist/polyfill.*.js",
