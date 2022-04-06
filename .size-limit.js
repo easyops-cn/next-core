cc189f95a65a4858d464586343620f9e5a784f05
Shenwei Wang <wangshenwei@qq.com>
2022-04-06 18:26:27 +0800

    chore(): increase size-limit for brick-dll

--- .size-limit.js
+++ .size-limit.js
@@ -21,7 +21,7 @@ module.exports = [
   },
   {
     path: "packages/brick-container/dist/dll.*.js",
-    limit: "1.39 MB",
+    limit: "1.40 MB",
   },
   {
     path: "packages/brick-container/dist/main.*.js",
