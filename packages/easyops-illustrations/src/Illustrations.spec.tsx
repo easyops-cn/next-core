749292ff65ee58477127d65997e4da74a8e6ae93
李佳明 <jimmyli@easyops.cn>
2022-06-10 16:18:43 +0800

    chore(): refine code

--- packages/easyops-illustrations/src/Illustrations.spec.tsx
+++ packages/easyops-illustrations/src/Illustrations.spec.tsx
@@ -1,4 +1,4 @@
-import { getIllustration } from "./Illustrations";
+import { getIllustration, determineIllustrationName } from "./Illustrations";
 
 describe("Illustration", () => {
   it("should work", () => {
@@ -31,12 +31,20 @@ describe("Illustration", () => {
 
   it("should work with dark theme", () => {
     window.CORE_ROOT = "-/core/";
-    const result = getIllustration({
-      name: "no-content",
-      theme: "dark-v2",
-    });
-
-    expect(result).toBe("-/core/assets/illustrations/mockFile");
+    let result = determineIllustrationName("default", "dark-v2", "no-content");
+    expect(result).toBe("no-content-dark");
+    result = determineIllustrationName("exception", "dark-v2", "no-content");
+    expect(result).toBe("no-content-dark");
+    result = determineIllustrationName("feedback", "dark-v2", "no-content");
+    expect(result).toBe("no-content-dark");
+    result = determineIllustrationName("easyops2", "dark-v2", "no-content");
+    expect(result).toBe("no-content-dark");
+    const result_not_dark = determineIllustrationName(
+      "not-easyops",
+      "dark-v2",
+      "some-illustrations"
+    );
+    expect(result_not_dark).toBe("some-illustrations");
   });
 
   it("should return undefined when illustration not exist", () => {
