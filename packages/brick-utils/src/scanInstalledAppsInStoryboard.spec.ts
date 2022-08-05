b310f8ae3a251ddef355784db35c8ec8007d253f
Shenwei Wang <wangshenwei@qq.com>
2022-08-05 11:08:33 +0800

    chore(): remove unused code

--- packages/brick-utils/src/scanInstalledAppsInStoryboard.spec.ts
+++ packages/brick-utils/src/scanInstalledAppsInStoryboard.spec.ts
@@ -71,33 +71,3 @@ describe("scanInstalledAppsInStoryboard", () => {
   });
 });
 
-// describe("scanPermissionActionsInAny", () => {
-//   it("should work", () => {
-//     const brickConf = {
-//       brick: "b-b",
-//       properties: {
-//         good: "<% PERMISSIONS.check('my:action-a') %>",
-//         good2: "<% () => PERMISSIONS.check('my:action-b', 'my:action-c') %>",
-//         good3:
-//           "<% PERMISSIONS.check('my:action-d') && PERMISSIONS.check('my:action-e') %>",
-//         bad: "<% PERMISSIONS.check(bad) %>",
-//         bad2: "<% PERMISSIONS.check(123) %>",
-//         bad3: "<% PERMISSIONS.doSomething('my:action-z') %>",
-//         bad4: "<% PERMISSIONS.check['my:action-y'] %>",
-//         bad5: "<% ANY.check('my:action-x') %>",
-//         bad6: "PERMISSIONS.check('my:action-w')",
-//         bad7: "<% PERMISSIONS.check() %>",
-//         bad8: "<% 'my:action-v' %>",
-//         bad9: "<% PERMISSIONS[check]('my:action-u') %>",
-//         bad10: "<% (PERMISSIONS) => PERMISSIONS.check('my:action-t') %>",
-//       },
-//     };
-//     expect(scanPermissionActionsInAny(brickConf).sort()).toEqual([
-//       "my:action-a",
-//       "my:action-b",
-//       "my:action-c",
-//       "my:action-d",
-//       "my:action-e",
-//     ]);
-//   });
-// });
