f1f98c5c769164a5b20b5c48b12d37d0d331d4aa
zekun <949177145@qq.com>
2022-11-07 12:13:38 +0800

    chore(): add minimap icon

--- .size-limit.js
+++ .size-limit.js
@@ -29,7 +29,7 @@ module.exports = [
   },
   {
     path: "packages/brick-container/dist/icons--*.js",
-    limit: "455 KB",
+    limit: "456 KB",
   },
   {
     path: "packages/brick-container/dist/polyfill.*.js",
