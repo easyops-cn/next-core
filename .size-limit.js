80b6d97e70155aef61a4debb25811bb47b965163
zekun <949177145@qq.com>
2023-01-03 11:24:29 +0800

    chore(): 添加模型icon refs SERVICE_OBSERVE-2040

--- .size-limit.js
+++ .size-limit.js
@@ -29,7 +29,7 @@ module.exports = [
   },
   {
     path: "packages/brick-container/dist/icons--*.js",
-    limit: "800 KB",
+    limit: "820 KB",
   },
   {
     path: "packages/brick-container/dist/polyfill.*.js",
