1a7377b06cbd164373bfbbaeb1454c0e93a053d0
janiceshi <297289441@qq.com>
2023-02-06 18:02:21 +0800

    feat(report_nb): next-core的report_brick_next_package.py修改， 将nb包的完整信息传送给micro_app_service Ref NEXT_BUILDER-2562

--- packages/brick-container/tools/report_installed_brick_next_package.py
+++ packages/brick-container/tools/report_installed_brick_next_package.py
@@ -6,6 +6,7 @@ import sys
 import ens_api
 import requests
 import simplejson
+import tarfile
 
 from copy import deepcopy
 
@@ -74,7 +75,7 @@ def collect(install_path):
   return package_name, bricks_content, stories_content, snippets_content, contract_content
 
 
-def report_bricks_atom(org, package_name, bricks_content, stories_content, snippets_content):
+def report_bricks_atom(org, nb_targz_path, package_name, package_version, bricks_content, stories_content, snippets_content):
   session_id, ip, port = ens_api.get_service_by_name("web.brick_next", "logic.micro_app_service")
   if session_id <= 0:
     raise NameServiceError("get nameservice logic.micro_app_service error, session_id={}".format(session_id))
@@ -82,14 +83,17 @@ def report_bricks_atom(org, package_name, bricks_content, stories_content, snipp
   headers = {"org": str(org), "user": "defaultUser"}
   # report atom
   atom_url = "http://{}/api/v1/brick/atom/import".format(address)
-  atom_param = {"packageName": package_name, "data": {"stories": stories_content, "bricks": bricks_content}}
-  rsp = requests.post(atom_url, json=atom_param, headers=headers)
+  data_dict = {"stories": stories_content, "bricks": bricks_content}
+  data_str = simplejson.dumps(data_dict)
+  data = {"packageName": package_name, "packageVersion": package_version, "data": data_str}
+  rsp = requests.post(atom_url, data=data, headers=headers, files={"file": open(nb_targz_path, "rb")})
   rsp.raise_for_status()
   # report snippet
   snippet_url = "http://{}/api/v1/brick/snippet/import".format(address)
   snippet_param = {"packageName": package_name, "snippets": snippets_content}
   rsp = requests.post(snippet_url, json=snippet_param, headers=headers)
   rsp.raise_for_status()
+
 
 def report_provider_into_contract(org, package_name, contract_content):
   session_id, ip, port = ens_api.get_service_by_name("web.brick_next", "logic.micro_app_service")
@@ -103,18 +107,32 @@ def report_provider_into_contract(org, package_name, contract_content):
   rsp = requests.post(url, json=param, headers=headers)
   rsp.raise_for_status()
 
+def mk_nb_tar_gz(output_filename, source_dir):
+  try:
+    with tarfile.open(output_filename, "w:gz") as tar:
+      tar.add(source_dir, arcname=os.path.basename(source_dir))
+    return True
+  except Exception as e:
+    logger.info(e)
+    return False
+
+
+def remove_tar_gz_file(nb_targz_path):
+  os.remove(nb_targz_path)
+
 
 if __name__ == "__main__":
   # 兼容老nb包install_postscript.sh调用report_installed_brick_next_package仅传入包路径
   if len(sys.argv) == 2:
     sys.exit(0)
 
-  if len(sys.argv) != 3:
-    print("Usage: ./report_installed_brick_next_package.py $org $install_path")
+  if len(sys.argv) != 4:
+    print("Usage: ./report_installed_brick_next_package.py $org $install_path $version")
     sys.exit(1)
 
   org = sys.argv[1]
   install_path = sys.argv[2]
+  package_version = sys.argv[3]
 
   if install_path.endswith(os.sep):
     install_path = install_path[:-1]
@@ -124,6 +142,13 @@ if __name__ == "__main__":
 
   package_name, bricks_content, stories_content, snippets_content, contract_content = collect(install_path)
   if package_name and bricks_content and snippets_content:
-    report_bricks_atom(org, package_name, bricks_content, stories_content, snippets_content)
+    targz_name = package_name + ".tar.gz"
+    nb_targz_path = os.path.join(os.path.dirname(install_path), targz_name)
+    if not mk_nb_tar_gz(nb_targz_path, install_path):
+      logger.error("mkdir tar.gz of nb err")
+      sys.exit(1)
+    report_bricks_atom(org, nb_targz_path, package_name, package_version, bricks_content, stories_content,
+                       snippets_content)
+    remove_tar_gz_file(nb_targz_path)
   if contract_content:
     report_provider_into_contract(org, package_name, contract_content)
