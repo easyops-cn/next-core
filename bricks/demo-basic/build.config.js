058e3378dac63764853f814d25569eeb63d6c928
weareoutman <wangshenwei@qq.com>
2023-02-14 16:47:18 +0800

    feat(): new brick: basic.fa-icon

--- bricks/demo-basic/build.config.js
+++ bricks/demo-basic/build.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 /** @type {import("@next-core/build-next-bricks").BuildNextBricksConfig} */
 export default {
-  extractCss: true,
+  // extractCss: true,
   moduleFederationShared: {
     react: {
       singleton: false,
