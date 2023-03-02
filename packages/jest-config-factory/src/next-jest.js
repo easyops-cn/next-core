09389a8b69665caee2e283e8dbcd9c61b41b066e
William Cai <williamcai@easyops.cn>
2023-03-02 15:33:26 +0800

    fix(next-jest): fix the value of jest option is incorrectly treated as target

--- packages/jest-config-factory/src/next-jest.js
+++ packages/jest-config-factory/src/next-jest.js
@@ -1,8 +1,13 @@
+const yargs = require("yargs");
 const { spawn } = require("child_process");
+const { options } = require(require
+  .resolve("jest-cli/package.json", { paths: [process.cwd()] })
+  .replace("package.json", "build/cli/args.js")); // Todo: Change to import from package, when https://github.com/facebook/jest/pull/13970 is released.
 const findFileUpward = require("./findFileUpward");
 
 const args = process.argv.slice(2);
-const target = args.find((a) => !a.startsWith("-"));
+const argv = yargs(args).options(options).argv;
+const target = argv._[0];
 
 function runByLerna() {
   spawn("npx", ["lerna", "run", "test", "--", ...args], {
