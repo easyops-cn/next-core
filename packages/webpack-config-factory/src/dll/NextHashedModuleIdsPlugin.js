ab73b128253af0d67ac8895b96435a8230311fd3
weareoutman <wangshenwei@qq.com>
2023-03-02 09:54:52 +0800

    fix(): refine dll to adopt v3

--- packages/webpack-config-factory/src/dll/NextHashedModuleIdsPlugin.js
+++ packages/webpack-config-factory/src/dll/NextHashedModuleIdsPlugin.js
@@ -25,7 +25,19 @@ class NextHashedModuleIdsPlugin {
   constructor(options) {
     if (!options) options = {};
 
-    validateOptions(schema, options, "Hashed Module Ids Plugin");
+    validateOptions(
+      {
+        ...schema,
+        properties: {
+          ...schema.properties,
+          migrateToBrickNextV3: {
+            type: "boolean",
+          },
+        },
+      },
+      options,
+      "Hashed Module Ids Plugin"
+    );
 
     /** @type {HashedModuleIdsPluginOptions} */
     this.options = Object.assign(
@@ -51,10 +63,46 @@ class NextHashedModuleIdsPlugin {
             for (const module of modules) {
               if (module.id === null && module.libIdent) {
                 // !!! Here's the replacement.
-                const id = module
-                  .libIdent({
-                    context: options.context || compiler.options.context,
-                  })
+                const originalId = module.libIdent({
+                  context: options.context || compiler.options.context,
+                });
+                const id = (
+                  this.options.migrateToBrickNextV3
+                    ? originalId
+                        .replace(
+                          "@next-core/brick-kit-v3",
+                          "@next-core/brick-kit"
+                        )
+                        .replace(
+                          "@next-core/brick-http-v3/index.esm.js",
+                          "@next-core/brick-http/dist/index.esm.js"
+                        )
+                        .replace(
+                          "/node_modules/@next-core/history-v3/index.esm.js",
+                          "/node_modules/history/esm/history.js"
+                        )
+                        .replace(
+                          "/node_modules/@next-core/lodash-v3/index.bundle.js",
+                          "/node_modules/lodash/lodash.js"
+                        )
+                        .replace(
+                          "/node_modules/@next-core/moment-v3/index.bundle.js",
+                          "/node_modules/moment/moment.js"
+                        )
+                        .replace(
+                          "/node_modules/@next-core/js-yaml-v3/index.esm.js",
+                          "/node_modules/js-yaml/index.js"
+                        )
+                        .replace(
+                          "/node_modules/@next-core/react-i18next-v3/index.esm.js",
+                          "/node_modules/react-i18next/dist/es/index.js"
+                        )
+                        .replace(
+                          "/node_modules/@next-core/i18next-v3/index.esm.js",
+                          "/node_modules/i18next/dist/esm/i18next.js"
+                        )
+                    : originalId
+                )
                   .replace("@next-core/", "@easyops/")
                   .replace(
                     "@babel/runtime/helpers/regeneratorRuntime",
