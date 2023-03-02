0305f2fddb2db2a0df2809b91722562562e18167
William Cai <williamcai@easyops.cn>
2023-03-02 17:08:38 +0800

    fix(next-jest): adaptation for folder restructuring of jest-cli
    ref: https://github.com/facebook/jest/pull/13395

--- packages/jest-config-factory/src/next-jest.js
+++ packages/jest-config-factory/src/next-jest.js
@@ -1,9 +1,15 @@
+const fs = require("fs");
 const yargs = require("yargs");
 const { spawn } = require("child_process");
-const { options } = require(require
-  .resolve("jest-cli/package.json", { paths: [process.cwd()] })
-  .replace("package.json", "build/cli/args.js")); // Todo: Change to import from package, when https://github.com/facebook/jest/pull/13970 is released.
 const findFileUpward = require("./findFileUpward");
+
+const jestCliBasePath = require
+  .resolve("jest-cli/package.json", { paths: [process.cwd()] })
+  .replace("/package.json", "");
+const jestCliArgsPath = `${jestCliBasePath}/build/args.js`;
+const { options } = require(fs.existsSync(jestCliArgsPath)
+  ? jestCliArgsPath
+  : `${jestCliBasePath}/build/cli/args.js`); // Todo: Change to import from package, when https://github.com/facebook/jest/pull/13970 is released.
 
 const args = process.argv.slice(2);
 const argv = yargs(args).options(options).argv;
