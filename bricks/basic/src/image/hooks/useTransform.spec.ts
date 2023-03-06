74609db653dd4028b39986633fa466a8ebd001b2
nlicroshan <nlicroshan@easyops.cn>
2023-03-06 15:12:36 +0800

    chore(): import renderHook from '@testing-library/react'

--- bricks/basic/src/image/hooks/useTransform.spec.ts
+++ bricks/basic/src/image/hooks/useTransform.spec.ts
@@ -1,10 +1,11 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, waitFor } from "@testing-library/react";
 import { describe, test, expect } from "@jest/globals";
 import useTransform from "./useTransform.js";
+import { act } from "react-dom/test-utils";
 
 describe("useTransform", () => {
   it("should work", async (): Promise<void> => {
-    const { result, waitForNextUpdate } = renderHook(() => useTransform());
+    const { result } = renderHook(() => useTransform());
 
     expect(result.current.transform).toEqual({
       translateX: 0,
@@ -17,51 +18,58 @@ describe("useTransform", () => {
       translateX: 10,
       rotate: 90,
     });
-    await waitForNextUpdate();
-    expect(result.current.transform).toEqual({
-      translateX: 10,
-      translateY: 0,
-      scale: 1,
-      rotate: 90,
+    await waitFor(() => {
+      expect(result.current.transform).toEqual({
+        translateX: 10,
+        translateY: 0,
+        scale: 1,
+        rotate: 90,
+      });
     });
 
     result.current.dispatchZoomChange(2.5);
-    await waitForNextUpdate();
-    expect(result.current.transform).toEqual({
-      translateX: 10,
-      translateY: 0,
-      scale: 2.5,
-      rotate: 90,
+    await waitFor(() => {
+      expect(result.current.transform).toEqual({
+        translateX: 10,
+        translateY: 0,
+        scale: 2.5,
+        rotate: 90,
+      });
     });
 
     result.current.dispatchZoomChange(100);
-    await waitForNextUpdate();
-    expect(result.current.transform).toEqual({
-      translateX: 10,
-      translateY: 0,
-      scale: 50,
-      rotate: 90,
+    await waitFor(() => {
+      expect(result.current.transform).toEqual({
+        translateX: 10,
+        translateY: 0,
+        scale: 50,
+        rotate: 90,
+      });
     });
 
     result.current.dispatchZoomChange(0.5);
-    await waitForNextUpdate();
-    expect(result.current.transform).toEqual({
-      translateX: 10,
-      translateY: 0,
-      scale: 25,
-      rotate: 90,
+    await waitFor(() => {
+      expect(result.current.transform).toEqual({
+        translateX: 10,
+        translateY: 0,
+        scale: 25,
+        rotate: 90,
+      });
     });
 
     result.current.dispatchZoomChange(0.0005);
-    await waitForNextUpdate();
-    expect(result.current.transform).toEqual({
-      translateX: 10,
-      translateY: 0,
-      scale: 1,
-      rotate: 90,
+    await waitFor(() => {
+      expect(result.current.transform).toEqual({
+        translateX: 10,
+        translateY: 0,
+        scale: 1,
+        rotate: 90,
+      });
     });
 
-    result.current.resetTransform();
+    act(() => {
+      result.current.resetTransform();
+    });
     expect(result.current.transform).toEqual({
       translateX: 0,
       translateY: 0,
