e72339246f4a066362d46433eb042abe36b23c69
weareoutman <wangshenwei@qq.com>
2023-03-16 19:39:29 +0800

    fix(): fix ReactUseBrick bug with inline refCallback

--- cypress/e2e/use-brick.spec.js
+++ cypress/e2e/use-brick.spec.js
@@ -16,16 +16,48 @@ for (const port of Cypress.env("ports")) {
       // so wait for specific content rendered.
       cy.contains("X:4:2");
       cy.contains("Modal:4:0");
+      cy.contains("Modal:S:8");
 
-      cy.expectMainContents(["X:Z:1X:Z:2", "X:3:1X:3:2X:4:1X:4:2", "Reset"]);
-      cy.expectPortalContents(["Modal:Z:0", "Modal:3:0", "Modal:4:0"]);
+      cy.expectMainContents([
+        "X:Z:1X:Z:2",
+        "X:3:1X:3:2X:4:1X:4:2",
+        "",
+        "Reset",
+      ]);
+      cy.expectPortalContents([
+        "Modal:S:7",
+        "Modal:S:8",
+        "Modal:Z:0",
+        "Modal:3:0",
+        "Modal:4:0",
+      ]);
 
       cy.contains("Reset").click();
 
-      cy.contains("X:6:2");
-      cy.contains("Modal:6:0");
-      cy.expectMainContents(["X:Z:1X:Z:2", "X:5:1X:5:2X:6:1X:6:2", "Reset"]);
-      cy.expectPortalContents(["Modal:Z:0", "Modal:5:0", "Modal:6:0"]);
+      cy.contains("X:5:2");
+      cy.contains("Modal:5:0");
+      cy.contains("Modal:S:9");
+      cy.expectMainContents([
+        "X:Z:1X:Z:2",
+        "X:3:1X:3:2X:5:1X:5:2",
+        "",
+        "Reset",
+      ]);
+      cy.expectPortalContents([
+        "Modal:S:7",
+        "Modal:Z:0",
+        "Modal:3:0",
+        "Modal:S:9",
+        "Modal:5:0",
+      ]);
+
+      cy.contains("Reset").click();
+
+      cy.contains("X:4:2");
+      cy.contains("Modal:4:0");
+      cy.contains("Modal:S:8");
+      cy.expectMainContents(["X:Z:1X:Z:2", "X:4:1X:4:2", "", "Reset"]);
+      cy.expectPortalContents(["Modal:Z:0", "Modal:S:8", "Modal:4:0"]);
     });
   });
 }
