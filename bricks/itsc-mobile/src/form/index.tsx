2e4232ce90c851316aad419481290c283981eaae
frankshi <8533334@qq.com>
2023-03-21 18:01:48 +0800

    feat(): add form component Refs ITSERVICE_NEXT-5898

---
+++ bricks/itsc-mobile/src/form/index.tsx
@@ -0,0 +1,193 @@
+import React, { useEffect } from "react";
+import { createDecorators, type EventEmitter } from "@next-core/element";
+import { ReactNextElement } from "@next-core/react-element";
+import { Layout, MessageBody } from "../interface.js";
+import { Form } from "antd-mobile";
+
+const { defineElement, property, event, method } = createDecorators();
+
+/**
+ * @id itsc-mobile.general-form
+ * @name itsc-mobile.general-form
+ * @docKind brick
+ * @description 通用输入框构件
+ * @author frank
+ * @noInheritDoc
+ */
+@defineElement("itsc-mobile.general-form", {
+  styleTexts: [],
+})
+class GeneralFormElement extends ReactNextElement {
+  readonly isFormElement = true;
+  #_values!: Record<string, unknown>;
+  formUtils: any;
+
+  set values(value: Record<string, unknown>) {
+    this.#_values = value;
+
+    this.#_setInitValue(value);
+  }
+  get values(): Record<string, unknown> {
+    return this.#_values;
+  }
+
+  #_setInitValue(values: Record<string, unknown>) {
+    this.formUtils.setFieldsValue(values);
+  }
+
+  /**
+   * @description 设置表单域内字段 id 的前缀
+   * @required false
+   * @group basic
+   */
+  @property() accessor name: string;
+
+  @property() accessor layout: Layout = "horizontal";
+
+  /**
+   * @description 表单样式
+   * @group ui
+   */
+  @property() accessor formStyle?: React.CSSProperties;
+
+  /**
+   * @detail
+   * @description
+   */
+  @event({ type: "values.change" }) accessor #valuesChangeEvent!: EventEmitter<
+    Record<string, unknown>
+  >;
+  handleValuesChange = (values: Record<string, unknown>) => {
+    this.#valuesChangeEvent.emit(values);
+  };
+
+  /**
+   * @description 表单验证成功时触发
+   */
+  @event({ type: "validate.success" }) accessor #successEvent!: EventEmitter<
+    Record<string, unknown>
+  >;
+  /**
+   * @description 表单验证报错时触发
+   */
+  @event({ type: "validate.error" }) accessor #errorEvent!: EventEmitter<
+    MessageBody[]
+  >;
+
+  /**
+   * @description
+   */
+  @method()
+  validate(): void {
+    this.lowLevelValidate();
+  }
+
+  lowLevelValidate(callback?: (params?: any) => void): void {
+    this.formUtils.validateFields((err, values) => {
+      if (err) {
+        this.#errorEvent.emit(err);
+      } else {
+        if (callback) {
+          callback(values);
+        }
+        this.#successEvent.emit(values);
+      }
+    });
+  }
+
+  /**
+   * @description
+   */
+  @method()
+  setInitValue(
+    values: Record<string, unknown>,
+    options?: { runInMicrotask?: boolean; runInMacrotask?: boolean }
+  ) {
+    if (options) {
+      options.runInMicrotask &&
+        queueMicrotask(() => {
+          this.#_setInitValue(values);
+        });
+      options.runInMacrotask &&
+        setTimeout(() => {
+          this.#_setInitValue(values);
+        });
+    } else {
+      this.#_setInitValue(values);
+    }
+  }
+
+  /**
+   * @description
+   */
+  @method()
+  resetFields(name?: string) {
+    this.formUtils.resetFields(name);
+  }
+
+  /**
+   * @description
+   */
+  @method()
+  getFieldsValue(options?: {
+    runInMicrotask?: boolean;
+    runInMacrotask?: boolean;
+  }) {
+    if (!(options?.runInMicrotask || options?.runInMacrotask)) {
+      return this.formUtils.getFieldsValue();
+    }
+    return new Promise((resolve, reject) => {
+      options.runInMicrotask &&
+        queueMicrotask(() => resolve(this.formUtils.getFieldsValue()));
+      options.runInMacrotask &&
+        setTimeout(() => resolve(this.formUtils.getFieldsValue()));
+    });
+  }
+
+  render() {
+    return (
+      <FormComponent
+        formElement={this}
+        name={this.name}
+        layout={this.layout}
+        onValuesChange={this.handleValuesChange}
+        formStyle={this.formStyle}
+      />
+    );
+  }
+}
+
+interface FormComponentProps {
+  name: string;
+  layout?: Layout;
+  formElement: any;
+  formStyle?: React.CSSProperties;
+  onValuesChange: (values: Record<string, unknown>) => void;
+}
+
+export function FormComponent(props: FormComponentProps) {
+  const { name, layout, formElement, formStyle, onValuesChange } = props;
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    formElement.formUtils = form;
+  }, [form]);
+
+  return (
+    <div className="form-container">
+      <Form
+        form={form}
+        name={name}
+        layout={layout}
+        style={{
+          ...formStyle,
+        }}
+        onValuesChange={(_, values) => onValuesChange?.(values)}
+      >
+        <slot></slot>
+      </Form>
+    </div>
+  );
+}
+
+export { GeneralFormElement };
--- bricks/itsc-mobile/src/interface.ts
+++ bricks/itsc-mobile/src/interface.ts
@@ -1,3 +1,10 @@
 /* ============== Input Type Start ============== */
 
 /* ============== Input Type End ============== */
+
+export type ComponentSize = "large" | "middle" | "small" | "xs";
+export type Layout = "horizontal" | "vertical";
+export interface MessageBody {
+  message: string;
+  type: string;
+}
