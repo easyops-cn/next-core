e15d59b3f3582125f811d7f7e31e1eb6595cad70
SheRunFeng <541899641@qq.com>
2023-05-09 18:46:31 +0800

    feat(): update getTracks unit test

---
+++ packages/runtime/src/internal/compute/getTracks.spec.ts
@@ -0,0 +1,39 @@
+import { getTracks } from "./getTracks.js";
+
+describe("getTracks", () => {
+  it("basic usage", () => {
+    const result1 = getTracks(
+      '<% "track context", CTX.abc + CTX["xyz"] + CTX[DATA.non] %>'
+    );
+    expect(result1).toEqual({
+      contextNames: new Set(["abc", "xyz"]),
+      formStateNames: false,
+      stateNames: false,
+    });
+
+    const result2 = getTracks(
+      '<%= CTX.abc + CTX["xyz"] + CTX[DATA.non] + STATE.a + STATE["b"] %>'
+    );
+    expect(result2).toEqual({
+      contextNames: ["abc", "xyz"],
+      formStateNames: false,
+      stateNames: ["a", "b"],
+    });
+  });
+
+  it("no track", () => {
+    const result1 = getTracks('<% "track context", DATA %>');
+    expect(result1).toEqual({
+      contextNames: false,
+      formStateNames: false,
+      stateNames: false,
+    });
+
+    const result2 = getTracks("<%= DATA.CTX.a %>");
+    expect(result2).toEqual({
+      contextNames: false,
+      formStateNames: false,
+      stateNames: false,
+    });
+  });
+});
