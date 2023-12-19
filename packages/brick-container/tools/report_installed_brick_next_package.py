3bd9f66e48f22ed2d64ff19a67b3d957cb0f3853
SheRunFeng <541899641@qq.com>
2023-12-19 11:05:42 +0800

    feat(): update report_install_brick_next_package

--- packages/brick-container/tools/report_installed_brick_next_package.py
+++ packages/brick-container/tools/report_installed_brick_next_package.py
@@ -88,8 +88,13 @@ def get_v3_story_provider(pr, examples_content, types_content):
 
     return story
 
+def find_item_by_story_id(lst, story_id):
+    for item in lst:
+        if item.get('storyId') == story_id:
+            return item
+    return None
 
-def get_v3_story(br, examples_content, types_content):
+def get_v3_story(br, examples_content, types_content, stories_content):
     story_id = br.get("name", "")
     if not story_id:
         return
@@ -108,8 +113,11 @@ def get_v3_story(br, examples_content, types_content):
     story["alias"] = br.get("alias", [])
     story["icon"] = br.get("icon")
 
-    if story_id in examples_content:
-        story["conf"] = examples_content[story_id]
+    story_item = find_item_by_story_id(stories_content, story_id)
+
+    if story_item != None:
+      story["conf"] = story_item.get("conf", [])
+      story["icon"] = story_item.get("icon")
 
     doc = br.get("doc", {})
     if story_id in types_content:
@@ -147,14 +155,18 @@ def collect_stories(install_path):
     manifest_path = os.path.join(install_path, "dist", "manifest.json")
     examples_path = os.path.join(install_path, "dist", "examples.json")
     types_path = os.path.join(install_path, "dist", "types.json")
+    bricks_path = os.path.join(install_path, "dist", "bricks.json")
+    with open(bricks_path) as bricks_file:
+        bricks_content = simplejson.load(bricks_file)
+    isV3Brick = bricks_content.get("id", False)
+    with open(stories_path) as stories_file:
+        stories_content = simplejson.load(stories_file)
     # v2 brick
-    if os.path.exists(stories_path):
-        with open(stories_path) as stories_file:
-            stories_content = simplejson.load(stories_file)
-            return stories_content
+    if isV3Brick == False:
+        return stories_content
     # v3 brick
-    elif os.path.exists(manifest_path):
-        stories_content = []
+    elif isV3Brick:
+        stories_list = []
         with open(manifest_path) as manifest_file:
             manifest_content = simplejson.load(manifest_file)
             examples_content = {}
@@ -166,14 +178,14 @@ def collect_stories(install_path):
                 with open(types_path) as types_file:
                     types_content = simplejson.load(types_file)
             for br in manifest_content.get("bricks", []):
-                story = get_v3_story(br, examples_content, types_content)
+                story = get_v3_story(br, examples_content, types_content, stories_content)
                 if story:
-                    stories_content.append(story)
+                    stories_list.append(story)
             for pr in manifest_content.get("providers", []):
                 story = get_v3_story_provider(pr, examples_content, types_content)
                 if story:
-                    stories_content.append(story)
-        return stories_content
+                    stories_list.append(story)
+        return stories_list
     return []
 
 
