c85f5515737853763ddac7c21c10dd77ded64d83
weareoutman <wangshenwei@qq.com>
2023-12-25 10:18:08 +0800

    fix(): fallback i18next to v22, which supports old browsers (es5)

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -2,10 +2,8 @@ import i18next from "i18next";
 
 export const i18n =
   process.env.NODE_ENV === "test"
-    ? i18next
-    : /* istanbul ignore next */ (
-        i18next as unknown as { default: typeof i18next }
-      ).default;
+    ? (i18next as unknown as typeof i18next.default)
+    : /* istanbul ignore next */ i18next.default;
 
 export * from "./init.js";
 export * from "./text.js";
