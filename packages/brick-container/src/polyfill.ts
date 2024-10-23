2f3e2a3eca77cfcbc4064c2424c3b6c3a977225a
weareoutman <wangshenwei@qq.com>
2024-10-23 18:20:07 +0800

    fix(): manually add array polyfills since old babel doesn't support them

--- packages/brick-container/src/polyfill.ts
+++ packages/brick-container/src/polyfill.ts
@@ -1,3 +1,7 @@
 import "core-js/stable";
+// Manually import some new polyfills since old babel doesn't support to load them automatically.
+import "core-js/es/array/to-sorted";
+import "core-js/es/array/to-reversed";
+import "core-js/es/array/to-spliced";
 import "regenerator-runtime/runtime";
 import "./replaceChildren";
