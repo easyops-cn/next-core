e338830721a06c2b9673ad901de050cc9eff0aa9
weareoutman <wangshenwei@qq.com>
2024-11-01 12:25:32 +0800

    fix(): fallback to versions that doesn't support optional chaining and nullish coalescing

--- packages/browserslist-config-next/production.js
+++ packages/browserslist-config-next/production.js
@@ -6,8 +6,15 @@ module.exports = [
   // with proper web components support: https://caniuse.com/custom-elementsv1
   // with flexbox gap support: https://caniuse.com/flexbox-gap
   // with css ::part support: https://caniuse.com/mdn-css_selectors_part
-  "Edge >= 84",
-  "Chrome >= 84",
-  "Firefox >= 72",
-  "Safari >= 14.1",
+  // Fallback to versions that doesn't support optional chaining and nullish coalescing
+  // Keep JS works with these versions, but some css may not work as expected
+  "Edge >= 79",
+  "Chrome >= 79",
+  "Firefox >= 71",
+  "Safari >= 13.0",
+  // General targets:
+  // "Edge >= 84",
+  // "Chrome >= 84",
+  // "Firefox >= 72",
+  // "Safari >= 14.1",
 ];
