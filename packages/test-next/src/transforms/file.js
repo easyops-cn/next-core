b5193f614fff267faa18b3aee1cbde31e2f919ea
Wang Shenwei <stevewang@easyops.cn>
2025-01-11 13:20:37 +0800

    fix(): fix react tests [skip ci]

--- packages/test-next/src/transforms/file.js
+++ packages/test-next/src/transforms/file.js
@@ -5,29 +5,20 @@ import { pascalCase } from "pascal-case";
 // https://github.com/facebook/create-react-app/blob/d960b9e38c062584ff6cfb1a70e1512509a966e7/packages/react-scripts/config/jest/fileTransform.js
 export default {
   /**
-   * @param {string} src
+   * @param {string} _src
    * @param {string} filename
    */
-  process(src, filename) {
+  process(_src, filename) {
     const assetFilename = JSON.stringify(path.basename(filename));
     if (filename.endsWith(".svg")) {
       const pascalCaseFilename = pascalCase(path.parse(filename).name);
       const componentName = `Svg${pascalCaseFilename}`;
       return {
-        code: `const React = require('react');
-          module.exports = {
-          __esModule: true,
-          default: React.forwardRef(function ${componentName}(props, ref) {
-            return {
-              $$typeof: Symbol.for('react.element'),
-              type: 'svg',
-              ref: ref,
-              key: null,
-              props: Object.assign({}, props, {
-                children: ${assetFilename}
-              })
-            };
-          }),
+        code: `import React from "react";
+          export default React.forwardRef(function ${componentName}(props, ref) {
+            return (
+              <svg {...props} ref={ref}>{${assetFilename}}</svg>
+            );
         };`,
       };
     }
