ec94491d54eaf74bc4766fad8e625fd274c63c5e
Wang Shenwei <stevewang@easyops.cn>
2025-01-12 13:42:01 +0800

    fix(i18n): make react-i18next as optional peer deps

--- packages/yo/src/templates/brick/i18n.ts
+++ packages/yo/src/templates/brick/i18n.ts
@@ -1,3 +1,5 @@
+import { i18n } from "@next-core/i18n";
+
 export enum K {}
 // HELLO = "HELLO",
 
@@ -12,6 +14,8 @@ const zh: Locale = {
 export const NS = "bricks/{{pkgName}}/{{>lastTagName}}";
 
 export const locales = { en, zh };
+
+export const t = i18n.getFixedT(null, NS);
 
 type Locale = { [k in K]: string } & {
   [k in K as `${k}_plural`]?: string;
