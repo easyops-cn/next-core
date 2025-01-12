bfae5bd748d42e666bbec04d81c3a1f274811ca1
Wang Shenwei <stevewang@easyops.cn>
2025-01-12 11:25:25 +0800

    fix(): fix svg mock

--- packages/test-next/src/transforms/file.js
+++ packages/test-next/src/transforms/file.js
@@ -14,12 +14,20 @@ export default {
       const pascalCaseFilename = pascalCase(path.parse(filename).name);
       const componentName = `Svg${pascalCaseFilename}`;
       return {
-        code: `import React from "react";
-          export default React.forwardRef(function ${componentName}(props, ref) {
-            return (
-              <svg {...props} ref={ref}>{${assetFilename}}</svg>
-            );
-        };`,
+        code: `"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+function _extends() { return _extends = Object.assign ? Object.assign.bind() : function (n) { for (var e = 1; e < arguments.length; e++) { var t = arguments[e]; for (var r in t) ({}).hasOwnProperty.call(t, r) && (n[r] = t[r]); } return n; }, _extends.apply(null, arguments); }
+var _default = exports.default = /*#__PURE__*/_react.default.forwardRef(function ${componentName}(props, ref) {
+  return /*#__PURE__*/_react.default.createElement("svg", _extends({}, props, {
+    ref: ref
+  }), ${assetFilename});
+});`,
       };
     }
     return {
