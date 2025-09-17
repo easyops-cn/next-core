dd286ffc4fea73efac6f50845e72f5381d2386eb
alexchen <alexchen@easyops.cn>
2025-09-17 17:13:27 +0800

    feat(): brick_next框架在生产运行时去掉console.xxx的输出
    Closes NEXT_BUILDER-5697

--- cypress/e2e/home.spec.js
+++ cypress/e2e/home.spec.js
@@ -32,7 +32,7 @@ for (const port of Cypress.env("ports")) {
     });
 
     it("work", () => {
-      cy.visit(`${origin}/next/home?lng=en`, {
+      cy.visit(`${origin}/next/home?lng=en&debugConsole=true`, {
         onBeforeLoad(win) {
           cy.spy(win.console, "error").as("console.error");
           cy.spy(win.console, "info").as("console.info");
