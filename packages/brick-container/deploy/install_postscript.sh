24a198b4d7827f4b2df49238067a9455c4201f77
yunxue <yunxue@easyops.cn>
2025-11-27 15:46:11 +0800

    fix(brick_next): 低发行版升级到 7.12，默认没有初始化 brick_next 的 clientId 和 secret；后续手动打开开关后，升级 NA/NB 组件时获取 token 失败 Refs INFRA-5390

--- packages/brick-container/deploy/install_postscript.sh
+++ packages/brick-container/deploy/install_postscript.sh
@@ -12,15 +12,11 @@ config_tool="${deploy_init_path}/tools/config_tool"
 
 export LD_LIBRARY_PATH=/usr/local/easyops/ens_client/sdk:${LD_LIBRARY_PATH}
 
-
-value=$(${config_tool} get --appID "deploy_init" --namespaceName "common" --key "check_auth_token.enable")
-if [[ ${value} == "true" ]]; then
-    # 初始化默认命名空间，并生成 clientId 和 secret
-    ${config_tool} init -f "${install_path}/conf/config.yaml"
-    if [[ $? -ne 0 ]];then
-       echo "import brick_next_v3 config namespace error, exit"
-       exit 1
-    fi
+# 初始化默认命名空间，并生成 clientId 和 secret
+${config_tool} init -f "${install_path}/conf/config.yaml"
+if [[ $? -ne 0 ]];then
+   echo "import brick_next config namespace error, exit"
+   exit 1
 fi
 
 
